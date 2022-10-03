38db0b01d4257cfd7ba0e435dd0c32958f7f6ef0
jinified <jinified@gmail.com>
2022-10-03 22:16:48 +0800

    Init landing screen and include repo

---
+++ lib/repositories/game_repository.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:waka/repositories/api_error.dart';
+
+class HttpGameRepository {
+  HttpGameRepository({required this.client});
+  final http.Client client;
+
+  Future<T> _getData<T>({
+    required Uri uri,
+    required T Function(dynamic data) builder,
+  }) async {
+    try {
+      final response = await client.get(uri);
+      switch (response.statusCode) {
+        case 200:
+          final data = json.decode(response.body);
+          return builder(data);
+        case 401:
+          throw const APIError.invalidApiKey();
+        case 404:
+          throw const APIError.notFound();
+        default:
+          throw const APIError.unknown();
+      }
+    } on SocketException catch (_) {
+      throw const APIError.noInternetConnection();
+    }
+  }
+}
+
+/// Providers used by rest of the app
+final weatherRepositoryProvider = Provider<HttpGameRepository>((ref) {
+  return HttpGameRepository(
+    client: http.Client(),
+  );
+});
--- lib/views/landing/landing_screen.dart
+++ lib/views/landing/landing_screen.dart
@@ -1,39 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class LandingScreen extends StatelessWidget {
+class LandingScreen extends StatefulWidget {
   const LandingScreen({Key? key}) : super(key: key);
 
-  void _onExploreButtonTap(BuildContext context) =>
-      GoRouter.of(context).push('/colors');
+  @override
+  LandingScreenState createState() {
+    return LandingScreenState();
+  }
+}
+
+class LandingScreenState extends State<LandingScreen> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    const backgroundColor = Color(0xFFF2EAE0);
+
     return Scaffold(
+      key: _formKey,
       body: Container(
         height: double.maxFinite,
         width: double.maxFinite,
         decoration: const BoxDecoration(
-            color: Colors.lightBlue
+            color: backgroundColor
         ),
-        child: Column(
+        child: FractionallySizedBox(
+            alignment: Alignment.center,
+            heightFactor: 0.3,
+            widthFactor: 0.5,
+            child: Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.orange,
+            ),
+            child:
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              height: 180,
-              width: 180,
-              child: Image.asset('assets/dash.png'),
+              height: 240,
+              width: 240,
+              child: Image.network('https://freesvg.org/img/mole2.png'),
             ),
             const SizedBox(height: 32),
-            Text(
-              'Enter your name',
-              style: Theme.of(context).textTheme.headline4?.copyWith(
-                    color: Colors.white,
-                  ),
+            Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ConstrainedBox(
+                    constraints: BoxConstraints.tight(const Size(300, 50)),
+                    child:
+                    TextFormField(
+                        validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                                return "Thou shall not enter without a name";
+                            }
+                            return (value.length > 12) ? "Please limit your name to 12 char" : null;
+                        },
+                        decoration: const InputDecoration(
+                                        icon: Icon(Icons.person),
+                                        border: UnderlineInputBorder(),
+                                        hintText: "Joe",
+                                        labelText: 'Enter your username',
+                                    ),
+                    ))),
+            const SizedBox(height: 32),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.white
+                ),
+                onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing Data')),
+                        );
+                    }
+                    GoRouter.of(context).push('/games');
+                },
+                child: const Text('Enter'),
             ),
-          ],
+            ],
         ),
-      ),
+        )
+      )),
     );
   }
 }
